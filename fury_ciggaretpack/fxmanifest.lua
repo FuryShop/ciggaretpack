4ad07f5551a067e58a9a10fd9aa6a5de94f4dd99
Fury Shop <95311122+FuryShop@users.noreply.github.com>
2024-10-11 22:09:45 +0200

    Add files via upload

---
+++ fury_ciggaretpack/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'Fury Shop'
+description 'fury_cigpack'
+version '1.0.0'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/fury_cigpack.ytyp'
+
+
+
+---If you want to use this in one of youre free scripts please make a attribution. Not allowed to use in any paid scripts
+---only non-commercial purposes.
+
